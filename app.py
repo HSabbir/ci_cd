c6321246a550dff1553571c43243ef3fd2ea5480
sabbir <bsse1014@iit.du.ac.bd>
2021-12-08 00:38:32 +0600

    edited port and procfile

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -28,5 +30,6 @@ def add(x,y):
     return x + y
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
